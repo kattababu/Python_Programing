d31c51c3b3979ca84ab9672f85a6b3a98bd042fc
Surendra-Worxogo <surendra@worxogo.com>
2020-07-09 23:51:40 +0530

    Basic Info of Python

---
+++ Range.py
@@ -0,0 +1,18 @@
+'''import os
+import sys'''
+
+
+print ("Python Range() Example")
+
+print ("Get Numbers from range  0 to 6")
+
+for i in range(6):
+    print(i, end=",")
+
+
+for i in range(5, 10):
+    print (i, end=",")
+
+
+for i in range(5, 10, 2):
+    print (i, end="\n")
